215ccb3ace0a7696d77e7e0f973a4d4c729c9354
Yamen-BENGUIRAT <yamenfut@gmail.com>
2023-09-17 11:38:59 +0200

    6

--- sizeof_types.c
+++ sizeof_types.c
@@ -4,8 +4,14 @@
 int main()
 {
     printf("size of char is :%lu\n", sizeof (char) );
-    return 0;
-    
     printf("size of short is :%lu\n", sizeof (short) );
+    printf("size of int is : %lu\n", sizeof (int));
+    printf("size of longint is : %lu\n", sizeof (long int));
+    printf("size of longlongint is : %lu\n", sizeof (long long int));
+    printf("size of float is : %lu\n", sizeof (float));
+    printf("size of double is : %lu\n", sizeof (double));
+    printf("size of long double is : %lu\n", sizeof (long double));
+
+
     return 0;
 }

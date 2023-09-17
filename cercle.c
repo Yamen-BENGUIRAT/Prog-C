4d48efd819e8f120411b278e0478dcba5cccf59f
Yamen-BENGUIRAT <yamenfut@gmail.com>
2023-09-17 11:07:16 +0200

    4

--- cercle.c
+++ cercle.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h> // Inclure la bibliothèque mathématique pour accéder à la constante pi
 
 int main()
 {
-    int r = 5;
-    printf("%d\n",r); // Utilisez printf pour afficher la valeur de r
+    int rayon = 5;
+    double Aire; // Déclarez une variable pour stocker l'aire du cercle
+    double Perimetre;
+
+    Aire = M_PI*rayon*rayon;
+    Perimetre = 2*M_PI*rayon;
+
+    printf("L'aire du cercle est :%lf\n",Aire); // Utilisez printf pour afficher la valeur de r
+    printf("Le perimetre du cercle est :%lf\n", Perimetre);
     return 0;
 }
 

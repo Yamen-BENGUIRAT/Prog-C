ec9c002ec6d2dde138501bde7c78c80aa33ffb92
Yamen-BENGUIRAT <yamenfut@gmail.com>
2023-09-17 20:46:54 +0200

    yahoo

--- opérateurs.c
+++ opérateurs.c
@@ -6,8 +6,14 @@ int main()
     int a = 16;
     int b = 3;
 
-    
 
+    printf("a+b =: %d\n", a+b);
+    printf("a-b =: %d\n", a-b);
+    printf("a*b =: %d\n", a*b);
+    printf("a/b =: %d\n", a/b);
+    printf("a%b =: %d\n", a%b);
+    printf("a<b =: %d\n", a<b);
+    printf("a>b =: %d\n", a>b);
 
     return 0;
 }

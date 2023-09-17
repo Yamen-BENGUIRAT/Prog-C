e15975659c149005e8309fbb2bed2e9da69ee798
Yamen-BENGUIRAT <yamenfut@gmail.com>
2023-09-17 11:20:15 +0200

    5

---
+++ sizeof_types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("size of char is :%lu\n", sizeof (char) );
+    return 0;
+    
+    printf("size of short is :%lu\n", sizeof (short) );
+    return 0;
+}

57e3536644dd71157547f0017ddc0eb6cd2582c7
Yamen-BENGUIRAT <yamenfut@gmail.com>
2023-09-27 08:48:26 +0200

    okkkay

---
+++ TP2/fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+int main() {
+
+
+    int Un;
+    int precedent = 0;
+    int suivant = 1;
+    scanf("Nombre de termes pour la suite ?%d", nombre)
+    for (n = 0; n =<b; n++){
+
+        Un = precedent - suivant;
+
+
+
+    }
+
+    return 0;
+}

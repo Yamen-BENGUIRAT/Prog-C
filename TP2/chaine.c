6d4e200594f6f802bbdcbc6f78c36992642210d0
Yamen-BENGUIRAT <yamenfut@gmail.com>
2023-09-27 11:44:42 +0200

    oui

--- TP2/chaine.c
+++ TP2/chaine.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     
-    scanf("%s" mot);
-    
+    int i;
+    char tall[100];
+    printf("Entrez une chaîne de caractères : ");
+    scanf("%s", tall);
+
+    int longueur = strlen(tall);
+    printf("La chaine \"%s\" contient %d caractères. \n", tall,longueur);
 
     return 0;
 }
+
+

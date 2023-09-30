fdd41ddb2e0913072bc1ec26594ff815a9752fc7
Yamen-BENGUIRAT <yamenfut@gmail.com>
2023-09-30 10:12:37 +0200

    pasmal

--- TP2/chaine.c
+++ TP2/chaine.c
@@ -11,26 +11,16 @@ int main() {
     int longueur = strlen(tall);
     printf("La chaine \"%s\" contient %d caractères. \n", tall,longueur);
 
-    char a[100] = "okkkay";
+    char a[200] = "okkkay";
     char b[100];
     printf("Entrez une chaîne de caractères :");
     scanf("%s", b);
-
-    strncpy(a,b, sizeof(b));
-    a[sizeof(b)-1] = '\0';
+    strcat(a, tall);
 
     printf("La chaîne a devient %s\n", a);
 
 
-
-
-
     return 0;
-
-
-
-
-
 
 }
 

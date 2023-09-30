a3de164b4aa54a85aca7661f7634397774b41e1a
Yamen-BENGUIRAT <yamenfut@gmail.com>
2023-09-30 10:52:12 +0200

    logic

---
+++ TP2/eudiant.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main() {
+    char name[2][12] = {
+        {"Yamen", "BENGUIRAT"}
+        {"Anass","BENGUIRAT" }
+    };
+
+    char adress[2][20] = {
+        {"Lyon", "Laennec"}
+        {" Grenoble", "VilleVerte"}
+    };
+
+    float notesC[] = {
+        {12,5}
+        {11}
+    };
+
+    float notesExploitation[] = {
+        {14}
+        {8,3}
+    };
+
+
+
+
+
+}
+

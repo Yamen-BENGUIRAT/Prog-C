e24d239dd74b281b221077d0b1ee16f0a1afa8fd
Yamen-BENGUIRAT <yamenfut@gmail.com>
2023-10-11 08:14:09 +0200

    logic

---
+++ TP3/grand_petit.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <time.h>
+
+
+int main() {
+
+    srand(time(NULL));
+
+    int itableau[100];
+    int plus_petit = 0;
+    int plus_grand = 899; 
+
+    for (i = 0, i<100, i++){
+        itableau[i] = rand()% +1;
+    } 
+
+    if tableau[i] < plus_petit;
+        plus_petit = tableau[i];
+
+    if tableau[i] > plus_grand;
+        plus_grand = tableau[i];
+
+
+    printf("");
+
+
+
+
+
+
+
+
+    return 0;
+
+
+
+
+
+
+}
+
---
+++ TP3/sizeof.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("size of int is :%lu\n", sizeof (int) );
+    printf("size of int* is :%lu\n", sizeof (int*) );
+    printf("size of int** is : %lu\n", sizeof (int**));
+    printf("size of char* is : %lu\n", sizeof (char*));
+    printf("size of char** is : %lu\n", sizeof (char**));
+    printf("size of char*** is : %lu\n", sizeof (char***));
+    printf("size of float* is : %lu\n", sizeof (float*));
+    printf("size of float** is : %lu\n", sizeof (float**));
+    printf("size of float*** is : %lu\n", sizeof (float***));
+
+
+
+
+    return 0;
+}
